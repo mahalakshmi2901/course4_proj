dbd28f3867dbf79b4a5164b8caaa2735ded0fd41
Mahalakshmi  Mahalakshmi <mahalakshmi.m.2018.it@rajalakshmi.edu.in>
2023-05-23 11:58:35 +0000

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -45,6 +45,7 @@ class Dev(Configuration):
       'movies',
       'gh',
       'django_celery_results',
+      'django_celery_beat',
   ]
   CELERY_RESULT_BACKEND = "django-db"
   CELERY_BROKER_URL = "redis://localhost:6379/0"
